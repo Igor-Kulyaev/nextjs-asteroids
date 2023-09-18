b2dff5eb23dcc4ddad0daf711c2fe4be320d4e57
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-09-18 15:05:52 +0300

    Add number formatter, pluralization

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,3 +5,36 @@ export const addUrlParams = (url: string, queryParams: Record<string, string>) =
   // Construct the URL with the query string
   return `${url}?${queryString}`;
 }
+
+export const convertDateToRusLocale = (
+  inputDate: string,
+) => {
+  const date = new Date(inputDate);
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+
+  return date.toLocaleDateString('ru-RU', options);
+};
+
+export const formatIntegerToRussianLocale = (number: number) => {
+  const roundedNumber = Math.round(number);
+  const formatter = new Intl.NumberFormat('ru-RU');
+  return formatter.format(roundedNumber);
+};
+
+export function formatLunarDistancePluralRus (number: number) {
+  const pluralRules = new Intl.PluralRules('ru-RU');
+  const forms = new Map([
+    ['one', 'лунная орбита'],
+    ['few', 'лунные орбиты'],
+    ['many', 'лунных орбит'],
+    ['other', 'лунных орбит'],
+  ]);
+
+  const pluralForm = pluralRules.select(number);
+  return `${formatIntegerToRussianLocale(number)} ${forms.get(pluralForm)}`;
+}
