a803d31bc87f2091e2c4cb15d22e5a4c38399ab7
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-09-18 15:04:43 +0300

    Format cart

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,11 +1,12 @@
 import cls from "./Cart.module.css";
+import {IAsteroidListItem} from "@/models/asteroidsListModel";
 
-export const Cart = () => {
+export const Cart = ({asteroidOrders}: {asteroidOrders: IAsteroidListItem[]}) => {
   return (
     <div className={cls.cartContainer}>
       <div className={cls.cartTop}>
         <p className={cls.cartTitle}>Корзина</p>
-        <p className={cls.cartCounter}>Кол-во астероидов</p>
+        <p className={cls.cartCounter}>Кол-во астероидов {asteroidOrders.length}</p>
       </div>
       <div className={cls.cartBtnWrapper}>
         <button className={cls.cartBtn}>Отправить</button>
