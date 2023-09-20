482f75dd42395a4ee38fe7bf380bb503b7577d30
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-09-20 16:46:13 +0300

    Add virtualization for asteroids list

---
+++ src/features/VirtualizedAsteroidsList/VirtualizedAsteroidsList.tsx
@@ -0,0 +1,117 @@
+import {IAsteroidListItem, IAsteroidsList} from "@/src/models/asteroidsListModel";
+import {Dispatch, SetStateAction, useCallback, useEffect, useRef, useState} from "react";
+import {DistanceSelector, ToastType} from "@/src/models/sharedModel";
+import {Virtuoso} from "react-virtuoso";
+import {useToastError} from "@/src/hooks/useToastError";
+import {Toast} from "@/src/shared/ui/Toast/Toast";
+import styles from "./VirtualizedAsteroidsList.module.css";
+import {AsteroidListItem} from "@/src/entities/AsteroidListItem/AsteroidListItem";
+import {Spinner} from "@/src/shared/ui/Spinner/Spinner";
+
+const VirtualizedSpinner = () => {
+  return (
+    <>
+      <div className={styles.spinnerWrapper}>
+        <Spinner />
+      </div>
+      <div style={{width: "100px", height: "10px"}}></div>
+    </>
+  )
+};
+
+
+
+export interface IAsteroidsListProps {
+  asteroids: IAsteroidsList;
+  setAsteroidOrders: Dispatch<SetStateAction<IAsteroidListItem[]>>;
+  distanceSelector: DistanceSelector;
+  setDistanceSelector: Dispatch<SetStateAction<DistanceSelector>>;
+}
+
+export default function VirtualizedAsteroidsList({
+ asteroids,
+ setAsteroidOrders,
+ distanceSelector,
+ setDistanceSelector
+}: IAsteroidsListProps) {
+
+  const nextPageLink = asteroids.links.next;
+  const [todayDateKey] = Object.keys(asteroids.near_earth_objects);
+  const asteroidsList = asteroids.near_earth_objects[todayDateKey];
+
+  const setKilometersDistance = () => setDistanceSelector(DistanceSelector.Kilometers);
+  const setLunarDistance = () => setDistanceSelector(DistanceSelector.Lunar);
+
+  const [newAsteroids, setNewAsteroids] = useState<IAsteroidListItem[]>([]);
+  const nextPaginationUrl = useRef(nextPageLink);
+
+  const {toastError, setToastError} = useToastError();
+
+  const getNewAsteroids = async () => {
+    try {
+      const result = await fetch(nextPaginationUrl.current);
+
+      if (result.status !== 200) {
+        throw new Error("Asteroids not found");
+      }
+
+      const data = await result.json();
+
+      const nextPageLink = data.links.next;
+      const [todayDateKey] = Object.keys(data.near_earth_objects);
+      const asteroidsList = data.near_earth_objects[todayDateKey];
+
+      setNewAsteroids(oldState => [...oldState, ...asteroidsList]);
+      nextPaginationUrl.current = nextPageLink;
+    } catch (error) {
+      setToastError(typeof error === "string" ? error : (error as Record<string, any>).message);
+    }
+  }
+
+  const loadMore = useCallback(() => {
+    return setTimeout(() => {
+      getNewAsteroids();
+    }, 200)
+  }, [getNewAsteroids])
+
+  useEffect(() => {
+    const timeout = loadMore()
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <>
+      <Toast message={toastError} type={ToastType.Error}/>
+      <div className={styles.asteroidsListWrapper}>
+        <h1 className={styles.asteroidsListTitle} >Ближайшие подлёты астероидов</h1>
+        <div className={styles.asteroidsDistanceButtonsWrapper}>
+          <button
+            className={`${styles.asteroidsDistanceBtn} ${distanceSelector === DistanceSelector.Kilometers && styles.asteroidsDistanceBtn_active}`}
+            onClick={setKilometersDistance}
+          >в километрах </button>
+          {" | "}
+          <button
+            className={`${styles.asteroidsDistanceBtn} ${distanceSelector === DistanceSelector.Lunar && styles.asteroidsDistanceBtn_active}`}
+            onClick={setLunarDistance}
+          > в лунных орбитах </button>
+        </div>
+        <Virtuoso
+          useWindowScroll
+          style={{ height: 600 }}
+          data={[...asteroidsList, ...newAsteroids]}
+          endReached={loadMore}
+          overscan={25}
+          itemContent={(index, asteroid) => {
+            return (
+              <AsteroidListItem
+                asteroid={asteroid}
+                setOrders={setAsteroidOrders}
+                distanceSelector={distanceSelector}/>
+            )
+          }}
+          components={{ Footer: VirtualizedSpinner }}
+        />
+      </div>
+    </>
+  )
+}
---
+++ src/pages/scroll.tsx
@@ -0,0 +1,62 @@
+import {IAsteroidsList} from "@/src/models/asteroidsListModel";
+import {format} from "date-fns";
+import {addUrlParams} from "@/src/shared/utils/utils";
+import {oneHour} from "@/src/shared/constants/constants";
+import {useAsteroidOrders} from "@/src/hooks/useAsteroidOrders";
+import {AsteroidsCart} from "@/src/features/AsteroidsCart/AsteroidsCart";
+import VirtualizedAsteroidsList from "@/src/features/VirtualizedAsteroidsList/VirtualizedAsteroidsList";
+
+export default function VirtualizedAsteroidsListScroll({asteroids}: {asteroids: IAsteroidsList}) {
+  const {
+    asteroidOrders,
+    setAsteroidOrders,
+    distanceSelector,
+    setDistanceSelector
+  } = useAsteroidOrders();
+
+  return (
+    <>
+      <VirtualizedAsteroidsList
+        asteroids={asteroids}
+        setAsteroidOrders={setAsteroidOrders}
+        distanceSelector={distanceSelector}
+        setDistanceSelector={setDistanceSelector}
+      />
+      <AsteroidsCart asteroidOrders={asteroidOrders} />
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const now = new Date();
+  const formattedDate = format(now, 'yyyy-MM-dd');
+
+  const urlWithParams = addUrlParams(process.env.API_URL as string, {
+    start_date: formattedDate,
+    end_date: formattedDate,
+    api_key: process.env.API_KEY as string,
+  });
+
+  try {
+    const response = await fetch(urlWithParams);
+
+    if (response.status !== 200) {
+      return {
+        notFound: true,
+      }
+    }
+
+    const data = await response.json();
+
+    return {
+      props: {
+        asteroids: data,
+      },
+      revalidate: oneHour,
+    }
+  } catch (error) {
+    return {
+      notFound: true
+    };
+  }
+}
