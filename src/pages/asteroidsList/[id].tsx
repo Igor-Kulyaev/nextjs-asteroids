041a5bcb6b80d9bcd54da5192b953cefa95abcb3
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-09-22 15:27:44 +0300

    Tested asteroid details without table

---
+++ src/pages/asteroidsList/[id].tsx
@@ -0,0 +1,124 @@
+import {useRouter} from "next/router";
+import {format} from "date-fns";
+import {addUrlParams} from "@/src/shared/utils/utils";
+import {oneHourInSeconds} from "@/src/shared/constants/constants";
+import {IAsteroidListItem} from "@/src/models/asteroidsListModel";
+import {IAsteroid} from "@/src/models/asteroidModel";
+import {GetServerSideProps, GetServerSidePropsContext, GetStaticProps, GetStaticPropsContext} from "next";
+import {AsteroidDetails} from "@/src/features/AsteroidDetails/AsteroidDetails";
+import {AsteroidAllDetails} from "@/src/features/AsteroidAllDetails/AsteroidAllDetails";
+
+export default function Asteroid({asteroid}: {asteroid: IAsteroid}) {
+  return <AsteroidAllDetails asteroid={asteroid} />
+}
+
+export const getStaticPaths = async () => {
+  const now = new Date();
+  const sevenDaysAhead = new Date(now);
+  sevenDaysAhead.setDate(now.getDate() + 7);
+
+  const todayFormattedDate = format(now, 'yyyy-MM-dd');
+  const sevenDayAheadFormattedDate = format(sevenDaysAhead, 'yyyy-MM-dd');
+
+  const urlWithParams = addUrlParams(process.env.API_URL as string, {
+    start_date: todayFormattedDate,
+    end_date: sevenDayAheadFormattedDate,
+    api_key: process.env.API_KEY as string,
+  });
+
+  try {
+    const response = await fetch(urlWithParams);
+
+    if (response.status !== 200) {
+      return {
+        paths: [],
+        fallback: "blocking",
+      };
+    }
+
+    const asteroidsSevenDayPeriod = await response.json();
+
+    const sevenDayPeriodAsteroidsList: IAsteroidListItem[] = [];
+    const sevenDayPeriodKeys = Object.keys(asteroidsSevenDayPeriod.near_earth_objects);
+
+    for (let key of sevenDayPeriodKeys) {
+      sevenDayPeriodAsteroidsList.push(asteroidsSevenDayPeriod.near_earth_objects[key]);
+    }
+
+    const paths = sevenDayPeriodAsteroidsList.map(({ id }) => ({
+      params: { id: id.toString() }
+    }));
+
+    return {
+      paths,
+      fallback: "blocking",
+    }
+  } catch (error) {
+    return {
+      paths: [],
+      fallback: "blocking",
+    };
+  }
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  console.log('context', context);
+
+  const urlWithParams = addUrlParams(`${process.env.API_URL as string}/neo/${context.params?.id}`, {
+    api_key: process.env.API_KEY as string,
+  });
+
+  try {
+    const response = await fetch(urlWithParams);
+
+    if (response.status !== 200) {
+      return {
+        notFound: true,
+      }
+    }
+
+    const data = await response.json();
+
+    return {
+      props: {
+        asteroid: data,
+      },
+    }
+  } catch (error) {
+    return {
+      notFound: true
+    };
+  }
+}
+
+// Define the new `getServerSideProps` function
+// export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+//   const asteroidId = context.params?.id as string;
+//
+//   // Fetch asteroid data based on the provided ID
+//   const urlWithParams = addUrlParams(`${process.env.API_URL as string}/neo/${asteroidId}`, {
+//     api_key: process.env.API_KEY as string,
+//   });
+//
+//   try {
+//     const response = await fetch(urlWithParams);
+//
+//     if (response.status !== 200) {
+//       return {
+//         notFound: true,
+//       };
+//     }
+//
+//     const data = await response.json();
+//
+//     return {
+//       props: {
+//         asteroid: data,
+//       },
+//     };
+//   } catch (error) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+// };
